bca1d480a9ab3d93a613931b6a5bba2248209362
pruebas cursoCivica <palakawino3@outlook.com>
2024-11-28 18:10:34 +0000

    incremental

--- models/stg_budget_products.sql
+++ models/stg_budget_products.sql
@@ -1,26 +1,17 @@
-{{ config(
-    materialized='incremental'
-    ) 
-    }}
+{{ config(materialized="incremental") }}
 
-WITH stg_budget_products AS (
-    SELECT * 
-    FROM {{ source('google_sheets','budget') }}
-    ),
+with
+    stg_budget_products as (select * from {{ source("google_sheets", "budget") }}),
 
-renamed_casted AS (
-    SELECT
-          _row
-        , month
-        , quantity 
-        , _fivetran_synced
-    FROM stg_budget_products
+    renamed_casted as (
+        select _row, month, quantity, _fivetran_synced from stg_budget_products
     )
 
-SELECT * FROM renamed_casted
+select *
+from renamed_casted
 
 {% if is_incremental() %}
 
-  where _fivetran_synced > (select max(_fivetran_synced) from {{ this }})
+    where _fivetran_synced > (select max(_fivetran_synced) from {{ this }})
 
 {% endif %}
